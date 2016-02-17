388e5cfb81ec5446135586570f17f9cbd4787fed
Samuel Cochran <sj26@sj26.com>
2016-02-17 15:04:46 +1100

    Deploy to s3 :rocket:

---
+++ .buildkite/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "--- :buildkite: Downloading artifacts"
+
+buildkite-agent artifact download "dist/*" "."
+
+echo "--- :s3: Deploying assets to $S3_BUCKET_PATH"
+
+pushd dist
+s3cmd put --acl-public --recursive --verbose --force --no-preserve "." "$S3_BUCKET_PATH"
+popd
+
+echo "All done! 💪"
