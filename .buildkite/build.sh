801ebef177afb1ab98e608f86bbe7916265bd10d
Samuel Cochran <sj26@sj26.com>
2016-02-17 12:22:31 +1100

    npm skips devDependencies in NODE_ENV=production
    https://github.com/npm/npm/blob/master/doc/cli/npm-install.md:
    
       With the --production flag (or when the NODE_ENV environment variable
       is set to production), npm will not install modules listed in
       devDependencies.

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -5,6 +5,6 @@ echo "--- :npm: Installing npm packages"
 rm -rf node_modules; npm-cache install npm
 
 echo "--- :webpack: Creating webpack assets"
-./node_modules/.bin/webpack -p --config webpack/config.js --progress --bail
+NODE_ENV=production ./node_modules/.bin/webpack -p --config webpack/config.js --progress --bail
 
 echo "All done! 💪"
